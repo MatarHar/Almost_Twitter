a9062cad361199682219dda968eab562dc764caa
PiotrTryfon <42303752+PiotrTryfon@users.noreply.github.com>
2018-12-05 23:14:19 +0100

    Doctrine part2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Almost_Twitter
+"# Kinda_Twitter" 
---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=280)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime", length=23)
+     */
+    private $time;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @return mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @return mixed $time
+     */
+    public function setTime($time): void
+    {
+        $this->time = $time;
+    }
+
+
+
+}
